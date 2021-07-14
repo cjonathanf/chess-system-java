eea5d3fbcb95816764c260b261ac11cfaed1e76a
Jonathan Ferreira <jooh.cferreira@gmail.com>
2021-07-14 20:10:29 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,37 @@
+package boardgame;
+
+public class Position {
+
+	private int raw;
+	private int column;
+
+	public Position() {
+
+	}
+
+	public Position(int raw, int column) {
+		this.raw = raw;
+		this.column = column;
+	}
+
+	public int getRaw() {
+		return raw;
+	}
+
+	public void setRaw(int raw) {
+		this.raw = raw;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return raw + ", " + column;
+	}
+}
